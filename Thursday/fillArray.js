bc34ef35829d9483c4bc3264b700cf6e780cc2d0
gms <gmmoose17@gmail.com>
2021-01-21 23:18:07 +0800

    ✨ updated

---
+++ Thursday/fillArray.js
@@ -0,0 +1,17 @@
+const arr = N => {
+    let element = [];
+    for (let i = 0; i < N; i++) {
+        element.push(i);
+    }
+    return element;
+    
+};
+
+/**
+ * Second Solution
+ */
+// const arr = (n) => [...Array(n).keys()];
+
+
+console.log(arr(), []);
+console.log(arr(4), [0,1,2,3]);
