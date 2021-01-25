f37bf346ce7f5fc5ecb0de6ad1555dfc4638f5f2
gms <gmmoose17@gmail.com>
2021-01-26 01:12:30 +0800

    :sparkles: updated

---
+++ Tuesday/abbreviateName.js
@@ -0,0 +1,14 @@
+const abbrevName = (name) => {
+    const n = name.split(' ');
+
+    // code away
+    return n.map((n) => n[0]).join('.').toUpperCase();
+
+};
+
+
+console.log(abbrevName('Sam Harris'), 'S.H');
+console.log(abbrevName('Patrick Feenan'), 'P.F');
+console.log(abbrevName('Evan Cole'), 'E.C');
+console.log(abbrevName('P Favuzzi'), 'P.F');
+console.log(abbrevName('David Mendieta'), 'D.M');
