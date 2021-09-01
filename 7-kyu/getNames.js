3dc6981ae85f440644f2663fe0d8f02760b88322
Gm Awesome <gmmoose17@gmail.com>
2021-09-01 23:24:58 +0800

    ✨ updated

--- 7-kyu/getNames.js
+++ 7-kyu/getNames.js
@@ -2,10 +2,13 @@
 //   return data.map(function(item){item.name});
 // }
 
-const getNames = (data) =>
-  data.map(function (item) {
-    item.name;
-  });
+// const getNames = (data) =>
+//   data.map(function (item) {
+//     return item.name;
+//   });
+
+// shortest
+const getNames = (data) => data.map((i) => i.name);
 
 console.log(1 + 1, 2);
 console.log(1 + 1, 2);
