eb1771be37d8f31dffca292a072f0f6d73795f7f
Gm Awesome <gmmoose17@gmail.com>
2021-09-27 22:59:48 +0800

    ✨ updated

--- 8-kyu/countByX.js
+++ 8-kyu/countByX.js
@@ -1,11 +1,15 @@
 const countBy = (x, n) => {
-  let i = 1;
+  // let i = 1;
   let z = [];
 
-  while (z.length < n) {
+  // while (z.length < n) {
+  //   z.push(x * i);
+  //   i++;
+  //   console.log(z.length);
+  // }
+
+  for (let i = 1; i < n; i++) {
     z.push(x * i);
-    i++;
-    console.log(z.length);
   }
   return z;
 };
