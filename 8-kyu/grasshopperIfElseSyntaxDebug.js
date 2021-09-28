0bcee5b9515c11bf9c9bf524b139cabb81318db4
Gm Awesome <gmmoose17@gmail.com>
2021-09-28 21:20:41 +0800

    ✨ updated

--- 8-kyu/grasshopperIfElseSyntaxDebug.js
+++ 8-kyu/grasshopperIfElseSyntaxDebug.js
@@ -6,7 +6,7 @@
 //   }
 // }
 
-const checkAlive = (health) => (health <= 0 ? false : true);
+const checkAlive = (health) => health > 0;
 
 console.log(checkAlive(5), true);
 console.log(checkAlive(0), false);
