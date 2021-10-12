f571e8a14ab165b610d616508d8d9ff0cc5331e9
Gm Awesome <gmmoose17@gmail.com>
2021-10-13 06:38:48 +0800

    ✨ updated

--- 8-kyu/removeDuplicatesFromList.js
+++ 8-kyu/removeDuplicatesFromList.js
@@ -1,4 +1,6 @@
-const distinct = (a) => Array.from(a);
+// const distinct = (a) => Array.from(a);
+
+const distinct = (a) => [...a];
 
 console.log(distinct([1]), [1]);
 console.log(distinct([1, 2]), [1, 2]);
